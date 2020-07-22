1972e224d403e5c3accc49e00b5dfb9b432b7409
saiesh-devp <jain.sai@northeastern.edu>
2020-07-22 09:32:15 -0400

    files cbunled

---
+++ ui/src/IssueFilter.jsx
@@ -0,0 +1,12 @@
+/* eslint linebreak-style: ["error", "windows"] */
+
+/* globals React */
+/* eslint "react/prefer-stateless-function": "off" */
+
+export default class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the issue filter.</div>
+    );
+  }
+}
---
+++ ui/webpack.config.js
@@ -0,0 +1,20 @@
+/* eslint linebreak-style: ["error", "windows"] */
+
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/App.jsx',
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
