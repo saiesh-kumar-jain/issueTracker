0e36d626252405e1f74f0081a118a602b7bf094f
saiesh-devp <jain.sai@northeastern.edu>
2020-07-23 10:20:45 -0400

    Drop down display value

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -2,6 +2,7 @@
 /* eslint "react/prefer-stateless-function": "off" */
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import URLSearchParams from 'url-search-params';
 
 class IssueFilter extends React.Component {
   constructor() {
@@ -19,11 +20,13 @@ class IssueFilter extends React.Component {
   }
 
   render() {
+    const { location: { search } } = this.props;
+    const params = new URLSearchParams(search);
     return (
       <div>
         Status:
         {' '}
-        <select onChange={this.onChangeStatus}>
+        <select value={params.get('status') || ''} onChange={this.onChangeStatus}>
           <option value="">(All)</option>
           <option value="New">New</option>
           <option value="Assigned">Assigned</option>
